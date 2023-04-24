adcab017124b0af80e93970a3ce2dbe805222f22
Elaidzha1940 <eli080293@gmail.com>
2023-04-24 12:38:52 +0700

    ButtonStyle + added a New File

--- ButtonMultiState/ButtonMultiState/ButtonMultiStateApp.swift
+++ ButtonMultiState/ButtonMultiState/ButtonMultiStateApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct ButtonMultiStateApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ButtonMultiState()
         }
     }
 }
---
+++ ButtonMultiState/ButtonMultiState/ButtonStyle.swift
@@ -0,0 +1,20 @@
+//
+//  ButtonStyle.swift
+//  ButtonMultiState
+//
+//  Created by Elaidzha Shchukin on 24.04.2023.
+//
+
+import SwiftUI
+
+struct ButtonStyle: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ButtonStyle_Previews: PreviewProvider {
+    static var previews: some View {
+        ButtonStyle()
+    }
+}
