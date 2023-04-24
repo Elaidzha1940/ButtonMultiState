2b8c382c13ef89a327bea40f484041e9db0e750b
Elaidzha1940 <eli080293@gmail.com>
2023-04-24 12:32:14 +0700

    ButtonMultiState

---
+++ ButtonMultiState/ButtonMultiState/ButtonMultiStateApp.swift
@@ -0,0 +1,17 @@
+//
+//  ButtonMultiStateApp.swift
+//  ButtonMultiState
+//
+//  Created by Elaidzha Shchukin on 24.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct ButtonMultiStateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
